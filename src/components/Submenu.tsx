1650ea459bd6e5ac9b98804d8b8234444c3e929b
Maciej <maciej.bodnar02@gmail.com>
2025-06-17 17:40:46 +0200

    new submenu items

--- src/components/Submenu.tsx
+++ src/components/Submenu.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { NavLink, To } from 'react-router-dom';
 
-interface SubmenuProps {}
+interface SubmenuProps {
+  link?: string;
+  children?: React.ReactNode;
+  label?: string;
+}
 
-export const Submenu = ({}: SubmenuProps) => {
-  return <div className="min-w-full h-full p-5">Submenu</div>;
+export const Submenu = ({ link, label, children }: SubmenuProps) => {
+  return (
+    <div className="min-w-full h-full p-2">
+      <NavLink
+        to={link as To}
+        className="flex p-4 text-gray-600 hover:text-gray-900  text-center items-center justify-center gap-2"
+      >
+        {children}
+        <span>{label}</span>
+      </NavLink>
+    </div>
+  );
 };
