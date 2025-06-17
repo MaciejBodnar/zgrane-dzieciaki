a099813b8212599d8effe7020851bb9fd1d984aa
Maciej <maciej.bodnar02@gmail.com>
2025-06-17 17:47:14 +0200

    link fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -239,7 +239,7 @@ const Navbar = () => {
               <div>
                 <div className="w-full text-gray-600 hover:text-gray-900 py-3 flex items-center justify-center space-x-2 border-b border-gray-200">
                   <FaInfoCircle className="text-blue-500" />
-                  <Link href="/o-nas">
+                  <Link to="/o-nas">
                     <span>O nas</span>
                   </Link>
                   <button onClick={() => setIsMobileAboutOpen(!isMobileAboutOpen)}>
@@ -268,7 +268,7 @@ const Navbar = () => {
               <div>
                 <div className="w-full text-gray-600 hover:text-gray-900 py-3 flex items-center justify-center space-x-2 border-b border-gray-200">
                   <FaList className="text-blue-500" />
-                  <Link href="/oferta">
+                  <Link to="/oferta">
                     <span>Oferta</span>
                   </Link>
                   <button onClick={() => setIsMobileDropdownOpen(!isMobileDropdownOpen)}>
